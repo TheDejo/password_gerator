50fc0c11b6e12d70bf6149fad5301e3f41b9b57d
TheDejo <65681287+TheDejo@users.noreply.github.com>
2021-02-07 19:41:30 +0100

    added javascript code

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,50 @@ import React from 'react';
 
 import  '../App.css'
 
+const resultEl = document.querySelector('#result')
+const lengthEl = document.querySelector('#length')
+const generateEl = document.querySelector('#generate')
+const uppercaseEl = document.querySelector('#uppercase')
+const lowercaseEl = document.querySelector('#result')
+const numbersEl = document.querySelector('#numbers')
+const symbolsEl = document.querySelector('#symbols')
+const clipboardEl = document.querySelector('#clipboard')
+
+
+
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value
+  const hasLower = lowercaseEl.checked
+  const hasUpper = uppercaseEl.checked
+  const hasNumber = numbersEl.checked
+  const hasSymbol = symbolsEl.checked
+  
+})
+
+const getRandomLower = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
+
+const getRandomUpper = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+const getRandomNumber = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 48)
+}
+
+const getRandomSymbol = () => {
+  const symbols = '!£$%^&*()_+@><'
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+}
+
 const App = () => {
   return (
     <div className="body">
@@ -24,15 +68,15 @@ const App = () => {
           </div>
           <div className="setting">
             <label>Include lowercase leters</label>
-            <input type="checkbox" id="uppercase" checked></input>
+            <input type="checkbox" id="lowercase" checked></input>
           </div>
           <div className="setting">
             <label>Include numbers </label>
-            <input type="checkbox" id="uppercase" checked></input> 
+            <input type="checkbox" id="numbers" checked></input> 
           </div>
           <div className="setting">
             <label>Include symbols </label>
-            <input type="checkbox" id="uppercase" checked></input>
+            <input type="checkbox" id="symbols" checked></input>
           </div>
         </div>
         <button className="btn btn-large" id="generate">Generate Password</button>
